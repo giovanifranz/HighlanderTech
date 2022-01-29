0c8a84c53257712948286de462609d5ec010b7e9
giovanifranz <giovanifranz@gmail.com>
2022-01-29 19:20:01 -0300

    feat components Contato page

--- src/components/Contato/Cards/index.tsx
+++ src/components/Contato/Cards/index.tsx
@@ -1,13 +1,5 @@
 import { Title, Container, Content } from './styles'
-import {
-  SUA_DUVIDA,
-  SUA_VISAO,
-  SEU_ORCAMENTO,
-  SEU_PLANO,
-  SEU_PROBLEMA,
-  SEU_SETUP,
-  SUA_NECESSIDADE
-} from './Models'
+import { SUA_DUVIDA, SEU_PLANO, SEU_SETUP } from './Models'
 export function Cards() {
   return (
     <Container>
