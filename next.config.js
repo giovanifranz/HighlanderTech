ed4cfd6f994debfcd19b295fb345c3c4354879dc
giovanifranz <giovanifranz151@gmail.com>
2022-07-26 21:06:03 -0300

    refactor: tailwind

--- next.config.js
+++ next.config.js
@@ -7,7 +7,5 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
+  swcMinify: true,
 });
