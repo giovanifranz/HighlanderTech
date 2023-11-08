2eaec95a77b811054ae7930ff0af1f56c2c1166b
giovanifranz <giovanifranz151@gmail.com>
2023-11-08 19:55:40 -0300

    feat: update zod schema

--- src/hooks/useForm/schema.ts
+++ src/hooks/useForm/schema.ts
@@ -1,13 +1,15 @@
 import { z } from 'zod'
 
 export const formSchema = z.object({
-  nome: z.string().nonempty('O nome é obrigatório'),
+  nome: z.string().min(1, 'O nome é obrigatório'),
   email: z
     .string()
-    .nonempty('O e-mail é obrigatório')
+    .min(1, 'O e-mail é obrigatório')
     .email()
     .transform((email) => email.toLowerCase()),
-  mensagem: z.string().nonempty('A mensagem é obrigatória'),
+  mensagem: z
+    .string()
+    .min(10, 'A mensagem deve conter pelo menos 10 carácteres'),
   telefone: z.string(),
   service: z.string(),
 })
